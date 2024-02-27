e0f3fd322c4548c54ec3f6cd1f163dfb0c14c92a
Andrey <111135827+mangystx@users.noreply.github.com>
2024-02-27 16:17:48 +0200

    Добавьте файлы проекта.

---
+++ Calculated/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Data;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculated
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			CultureInfo customCulture = new CultureInfo("en-US");
+			customCulture.NumberFormat.NumberDecimalSeparator = ".";
+			Thread.CurrentThread.CurrentCulture = customCulture;
+			Thread.CurrentThread.CurrentUICulture = customCulture;
+
+			foreach (var el in ButtonsGrid.Children)
+			{
+				if (el is Button btn)
+				{
+					btn.Click += ButtonClick;
+				}
+			}
+		}
+
+		private void ButtonClick(object sender, RoutedEventArgs e)
+		{
+			var symbol = ((Button)e.OriginalSource).Content.ToString();
+
+			if (symbol == "C")
+			{
+				output.Clear();
+			}
+			else if (symbol == "⌫")
+			{
+				if (output.Text.Length != 0)
+				{
+					output.Text = output.Text[..^1];
+				}
+			}
+			else if (symbol == "CE")
+			{
+                for (var i = output.Text.Length - 1; i >= 0; i--)
+                {
+					if (output.Text[i] == '-' || output.Text[i] == '+' || output.Text[i] == '*' || output.Text[i] == '/')
+					{
+						output.Text = output.Text[..i];
+						break;
+					}
+                }
+            }
+			else if (symbol == "=")
+			{
+				if (output.Text.Trim().Split().Length > 2) 
+				{
+					output.Text = new DataTable().Compute(output.Text, null).ToString();
+				}
+			}
+			else if(symbol == "㊂")
+			{
+				//later
+			}
+			else if (symbol == "-" || symbol == "+" || symbol == "*" || symbol == "/")
+			{
+				if (output.Text.Length != 0) 
+				{
+					if (output.Text[^1] == '-' || output.Text[^1] == '+' || output.Text[^1] == '*' || output.Text[^1] == '/')
+					{
+						output.Text = output.Text[..^1] + symbol;
+					}
+					else
+					{
+						output.Text += $" {symbol} ";
+					}
+				}
+			}
+			else
+			{
+				output.Text += symbol;
+			}
+		}
+	}
+}
