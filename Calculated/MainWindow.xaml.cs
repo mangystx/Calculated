fb523e5fbfaef0a4b6a3690a611eb9d0a654a025
Satilayzer <120048580+Satilayzer@users.noreply.github.com>
2024-03-03 20:20:08 +0200

    Update MainWindow.xaml.cs

--- Calculated/MainWindow.xaml.cs
+++ Calculated/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Data;
+using System.Data;
 using System.Globalization;
 using System.Text;
 using System.Windows;
@@ -13,84 +13,89 @@ using System.Windows.Shapes;
 
 namespace Calculated
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
 
-			CultureInfo customCulture = new CultureInfo("en-US");
-			customCulture.NumberFormat.NumberDecimalSeparator = ".";
-			Thread.CurrentThread.CurrentCulture = customCulture;
-			Thread.CurrentThread.CurrentUICulture = customCulture;
+            CultureInfo customCulture = new CultureInfo("en-US");
+            customCulture.NumberFormat.NumberDecimalSeparator = ".";
+            Thread.CurrentThread.CurrentCulture = customCulture;
+            Thread.CurrentThread.CurrentUICulture = customCulture;
 
-			foreach (var el in ButtonsGrid.Children)
-			{
-				if (el is Button btn)
-				{
-					btn.Click += ButtonClick;
-				}
-			}
-		}
-
-		private void ButtonClick(object sender, RoutedEventArgs e)
-		{
-			var symbol = ((Button)e.OriginalSource).Content.ToString();
-
-			if (symbol == "C")
-			{
-				output.Clear();
-			}
-			else if (symbol == "⌫")
-			{
-				if (output.Text.Length != 0)
-				{
-					output.Text = output.Text[..^1];
-				}
-			}
-			else if (symbol == "CE")
-			{
-                for (var i = output.Text.Length - 1; i >= 0; i--)
+            foreach (var el in ButtonsGrid.Children)
+            {
+                if (el is Button btn)
                 {
-					if (output.Text[i] == '-' || output.Text[i] == '+' || output.Text[i] == '*' || output.Text[i] == '/')
-					{
-						output.Text = output.Text[..i];
-						break;
-					}
+                    btn.Click += ButtonClick;
                 }
             }
-			else if (symbol == "=")
-			{
-				if (output.Text.Trim().Split().Length > 2) 
-				{
-					output.Text = new DataTable().Compute(output.Text, null).ToString();
-				}
-			}
-			else if(symbol == "㊂")
-			{
-				//later
-			}
-			else if (symbol == "-" || symbol == "+" || symbol == "*" || symbol == "/")
-			{
-				if (output.Text.Length != 0) 
-				{
-					if (output.Text[^1] == '-' || output.Text[^1] == '+' || output.Text[^1] == '*' || output.Text[^1] == '/')
-					{
-						output.Text = output.Text[..^1] + symbol;
-					}
-					else
-					{
-						output.Text += $" {symbol} ";
-					}
-				}
-			}
-			else
-			{
-				output.Text += symbol;
-			}
-		}
-	}
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var symbol = ((Button)e.OriginalSource).Content.ToString();
+
+            if (symbol == "C")
+            {
+                output.Clear();
+            }
+            else if (symbol == "⌫")
+            {
+                if (output.Text.Length != 0)
+                {
+                    output.Text = output.Text[..^1];
+                }
+            }
+            else if (symbol == "CE")
+            {
+                for (var i = output.Text.Length - 1; i >= 0; i--)
+                {
+                    if (output.Text[i] == '-' || output.Text[i] == '+' || output.Text[i] == '*' || output.Text[i] == '/')
+                    {
+                        output.Text = output.Text[..i];
+                        break;
+                    }
+                }
+            }
+            else if (symbol == "=")
+            {
+                if (output.Text.Trim().Split().Length > 2)
+                {
+                    output.Text = new DataTable().Compute(output.Text, null).ToString();
+                }
+            }
+
+            else if (symbol == "-" || symbol == "+" || symbol == "*" || symbol == "/")
+            {
+                if (output.Text.Length != 0)
+                {
+                    if (output.Text[^1] == '-' || output.Text[^1] == '+' || output.Text[^1] == '*' || output.Text[^1] == '/')
+                    {
+                        output.Text = output.Text[..^1] + symbol;
+                    }
+                    else
+                    {
+                        output.Text += $" {symbol} ";
+                    }
+                }
+            }
+            else if (symbol == "㊂")
+            {
+                this.Hide();
+                CalculatorWithExtraColumn extraCalculator = new CalculatorWithExtraColumn();
+                extraCalculator.Left = this.Left;
+                extraCalculator.Top = this.Top;
+                extraCalculator.Show();
+            }
+            else
+            {
+                output.Text += symbol;
+            }
+        }
+    }
 }
